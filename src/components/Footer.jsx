db70b72a3c878cb878b35dd98432aa8696a5e45c
Sol-Data <leonswork1007@gmail.com>
2025-04-18 11:47:22 +0200

    Complete the footer area

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,92 @@
 import React from "react";
 
+const footerText = [
+    {
+        title: "Front-End Projects",
+        desc: "You can see projects related to the Front-End",
+        link: "https://github.com/Sol-Data/Portfolio-Site-Vite.git",
+    },
+    {
+        title: "Backend Projects",
+        desc: "You can see projects related to the Backend",
+        link: "https://github.com/Sol-Data",
+    },
+    {
+        title: "Java Projects",
+        desc: "You can see projects related to the Java",
+        link: "https://github.com/Sol-Data",
+    },
+    {
+        title: "Kotlin Projects",
+        desc: "You can see projects related to the Kotlin",
+        link: "https://github.com/Sol-Data",
+    },
+    {
+        title: "Python Projects",
+        desc: "You can see projects related to the Python",
+        link: "https://github.com/Sol-Data",
+    },
+    {
+        title: "Database Projects",
+        desc: "You can see projects related to the Database",
+        link: "https://github.com/Sol-Data",
+    },
+    {
+        title: "Data Engineering Projects",
+        desc: "You can see projects related to the Data Engineering",
+        link: "https://github.com/Sol-Data",
+    },
+    {
+        title: "Data Science and Data Analysis Projects",
+        desc: "You can see projects related to the Data Science and Data Analysis",
+        link: "https://github.com/Sol-Data",
+    },
+    {
+        title: "Big Data Projects",
+        desc: "You can see projects related to the Big Data",
+        link: "https://github.com/Sol-Data",
+    },
+    {
+        title: "Artificial Intelligence Projects",
+        desc: "You can see projects related to the Artificial Intelligence",
+        link: "https://github.com/Sol-Data",
+    },
+];
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer id="footer" role="contentinfo">
+           <div className="footer__inner">
+              <h2 className="footer__text">
+                 <div>Portfolio-Project</div>
+                 <div>From IT Developer</div>
+              </h2>
+              <div className="footer__info">
+                 <div className="left">
+                    <div className="title">
+                        <a href="#">Sign Up</a>
+                    </div>
+                    <p className="desc">If you sign up for membership, you can use the board and comment function.</p>
+                 </div>   
+                 <div className="right">
+                    <h3>Portfolio Review</h3>
+                    <ul>
+                        {footerText.map((footer, key) => (
+                            <li key={key}>
+                                <a href={footer.link}>{footer.title}</a>
+                                <em>{footer.desc}</em>
+                            </li>
+                        ))}
+                     </ul>
+                </div>
+              </div> 
+              <div className="footer__right">
+                &copy; 2025 IT Developer<br />
+                This site was created using React
+              </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;

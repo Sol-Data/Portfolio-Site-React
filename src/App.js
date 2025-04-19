3a1b733badd178ed4c8872f06ec8010e53aa8e96
Sol-Data <leonswork1007@gmail.com>
2025-04-19 13:16:17 +0200

    The first version

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   }, []);
   
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
        <Routes>
           <Route path="/" element={<HomeView />} />
        </Routes>
